5ae1c35ccafc9c0d9f528dc4dd953a4bf548be05
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-05-24 22:02:02 -0700

    push

--- server.js
+++ server.js
@@ -9,27 +9,22 @@ var cheerio = require('cheerio');
 var mongoose = require('mongoose');
 var path = require('path');
 
-// Require all models
-var db = require("./models");
-
-// Port
 var PORT = process.env.PORT || 3000
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/pinkbikedb";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 var app = express();
 
-// Use morgan logger for logging requests
-// app.use(logger("dev"));
-// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+
+
+var db = require("./models");
+
 
 
 // Database configuration
